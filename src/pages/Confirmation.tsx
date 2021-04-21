771b74d796aaa638e56205c011abf8c3b903fa8f
Matheus Ishiyama <semrumo3@hotmail.com>
2021-04-21 15:59:09 -0300

    Aula 3

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
 import { Button } from "../components/Button";
 
@@ -7,6 +8,12 @@ import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export function Confirmation() {
+    const navigation = useNavigation();
+
+    function handleMoveOn() {
+        navigation.navigate("PlantSelect");
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
@@ -17,7 +24,7 @@ export function Confirmation() {
                     plantinhas com muito cuidado.
                 </Text>
                 <View style={styles.footer}>
-                    <Button title="Começar" />
+                    <Button title="Começar" onPress={handleMoveOn} />
                 </View>
             </View>
         </SafeAreaView>
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Welcome } from "../pages/Welcome";
 import { UserIdentification } from "../pages/UserIdentification";
 import { Confirmation } from "../pages/Confirmation";
+import { plantSelect } from "../pages/PlantSelect";
 
 import colors from "../styles/colors";
 
@@ -15,11 +16,15 @@ const AppRoutes: React.FC = () => (
         screenOptions={{ cardStyle: { backgroundColor: colors.white } }}
     >
         <stackRoutes.Screen name="Welcome" component={Welcome} />
+
         <stackRoutes.Screen
             name="UserIdentification"
             component={UserIdentification}
         />
+
         <stackRoutes.Screen name="Confirmation" component={Confirmation} />
+        
+        <stackRoutes.Screen name="PlantSelect" component={plantSelect} />
     </stackRoutes.Navigator>
 );
 
